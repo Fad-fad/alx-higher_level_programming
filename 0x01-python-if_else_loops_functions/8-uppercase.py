963eae18824652b019cf9417326141df76e4122d
Fad-fad <faduteimi@gmail.com>
2023-06-06 13:53:48 -0700

    ii

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def uppercase(str):
     for char in str:
-        if ord('a') <= ord(char) <= ord('z'):
-            print("{:s}".format(chr(ord(char) - 32)), end="")
-        else:
-            print(char, end="")
+        print("{:s} ".format(chr(ord(char) & 0xdf)), end="")
+    print()
