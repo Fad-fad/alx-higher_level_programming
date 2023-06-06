6e76af109cd05ef5a706add98757a03172468aa6
Fad-fad <faduteimi@gmail.com>
2023-06-06 04:58:29 -0700

    ii

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+unite = abs(number) % 10
+if int(unite) > 5:
+    print("Last digit of ",number, "is", unite," and is greater than 5")
+elif int(lastofdigit) == 0:
+    print("Last digit of ",number, "is", unite," and is 0")
+elif int(lastofdigit) < 6 and not 0:
+    print("Last digit of ",number, "is", unite," and is less than 6 and not 0")
+
+
+
