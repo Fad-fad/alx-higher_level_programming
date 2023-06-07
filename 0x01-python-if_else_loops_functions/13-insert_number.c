ad05bdc3dd17b24edb61ec2ab290915c6c221a89
Fad-fad <faduteimi@gmail.com>
2023-06-07 04:04:05 -0700

    ii

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,21 @@
+listint_t *insert_node(listint_t **head, int number)
+{
+listint_t *new;
+listint_t *current = *head;
+listint_t *precedent = *head;
+new = malloc(sizeof(listint_t));
+if (new == NULL)
+return (NULL);
+new->n = number;
+if (current->n < number)
+{
+precedent = current;
+current = current->next;
+}
+else
+{
+new->next = current
+precedent->next = new
+}
+return (new)
+}
