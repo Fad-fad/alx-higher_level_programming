5ad9504467e03656d393bcc2ea53d0853586a257
Fad-fad <faduteimi@gmail.com>
2023-06-14 15:21:48 -0700

    ii

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,5 +2,10 @@
 def best_score(a_dictionary):
     if a_dictionary is None or len(a_dictionary) == 0:
         return None
-    best_key = max(a_dictionary, key = a_dictionary.get)
+    max_score = float('-inf')  # Initialize with negative infinity
+    best_key = None
+    for key, value in a_dictionary.items():
+        if value > max_score:
+            max_score = value
+            best_key = key
     return best_key
