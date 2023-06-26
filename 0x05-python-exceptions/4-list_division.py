81f2a41fa7c8f8a4ed206c912857f5994fb81629
Fad-fad <faduteimi@gmail.com>
2023-06-26 10:23:53 -0700

    ii

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    result = []
+    for i in range(list_length):
+        try:
+            dividend = my_list_1[i]
+            divisor = my_list_2[i]
+            if not isinstance(dividend, (int, float)) or not isinstance(divisor, (int, float)):
+                raise TypeError("wrong type")
+            result.append(dividend / divisor)
+        except ZeroDivisionError:
+            result.append(0)
+            print("division by 0")
+        except IndexError:
+            result.append(0)
+            print("out of range")
+        finally:
+            pass
+    return result
