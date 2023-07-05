8f65b1c935ce60daecffb3a70b6742b9c1df53c5
Fad-fad <faduteimi@gmail.com>
2023-07-05 10:52:53 -0700

    ii

--- 0x08-python-more_classes/101-rectangle.py
+++ 0x08-python-more_classes/101-rectangle.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 """Solves the N-queens puzzle.
 
-Determines all possible solutions to placing 
+Determines all possible solutions to placing
+
+Example:
+    $ ./101-nqueens.py N
+
 N must be an integer greater than or equal to 4.
 
 Attributes:
